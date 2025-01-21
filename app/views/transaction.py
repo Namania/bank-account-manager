61f076c46cf324abc918b01a593c6bccc09dfff6
Namania <mael.namania@gmail.com>
2025-01-21 15:59:38 +0100

    :heavy_plus_sign: transaction system

--- app/views/transaction.py
+++ app/views/transaction.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.models import User
-from app.models import Account
+from app.models import Account, Transaction
 
 def newTransactionView(request):
     if "username" not in request.session.keys():
@@ -8,7 +8,15 @@ def newTransactionView(request):
     userId = request.session["id"]
     user = get_object_or_404(User, pk=userId)
 
-    accountId = int(request.GET["id"]) if request.method == "GET" and "id" in request.GET.keys() and request.GET["id"].isnumeric() else ""
+    if request.method == "POST":
+        sender = Account.objects.get(pk=request.POST["sender"])
+        receiver = Account.objects.get(pk=request.POST["receiver"])
+        amount = request.POST["amount"]
+        comment = request.POST["description"]
 
+        Transaction.objects.create(sender=sender, receiver=receiver, amount=amount, comment=comment)
+        return redirect("index")
+
+    accountId = int(request.GET["id"]) if request.method == "GET" and "id" in request.GET.keys() and request.GET["id"].isnumeric() else ""
     accounts = Account.objects.filter(owner=user).order_by("balance").reverse()
     return render(request, "app/new-transaction.html", {"accounts": accounts, "accountId": accountId})
