71c80ea759a08ad51aa90bbcc4cdd13403ea79e9
Namania <mael.namania@gmail.com>
2025-02-11 17:51:06 +0100

    :construction: static root & ignore them

--- bankAccountManager/settings.py
+++ bankAccountManager/settings.py
@@ -130,6 +130,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'bankAccountManager/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
