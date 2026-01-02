fdf0cd87889b670f871e08a8a6d83b09309b1181
Namania <mael.namania@gmail.com>
2026-01-02 18:17:39 +0100

    :recycle: Cote

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bankAccountManager.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bankAccountManager.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -14,5 +14,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
